6fb5f960bc1f9d617831b7d01eee8884beef3332
Ryan Connolly <rynam0@gmail.com>
2016-08-02 16:16:08 -0400

    change from baseURL to rootURL

--- blueprints/ember-cli-vtkui/files/config/environment.js
+++ blueprints/ember-cli-vtkui/files/config/environment.js
@@ -4,7 +4,7 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: '<%= dasherizedPackageName %>',
     environment: environment,
-    baseURL: '/',
+    rootURL: '/',
     locationType: 'hash',
     autoboot: true,
     EmberENV: {
@@ -30,7 +30,6 @@ module.exports = function(environment) {
 
   if (environment === 'test') {
     // Testem prefers this...
-    ENV.baseURL = '/';
     ENV.locationType = 'none';
 
     // keep test console output quieter
