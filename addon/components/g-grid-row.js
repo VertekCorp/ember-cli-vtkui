eda8544f48b1b30be8b61c5a9446a134af2ce1ca
Ryan Connolly <rynam0@gmail.com>
2016-08-02 14:31:19 -0400

    fix some jshint tests, upgrade some dependencies to address deprecations

--- addon/components/g-grid-row.js
+++ addon/components/g-grid-row.js
@@ -4,6 +4,6 @@ export default Ember.Component.extend({
   tagName: 'tr',
   classNameBindings: ['isOdd:tr-odd:tr-even'],
   isOdd: Ember.computed('rowIndex', function() {
-    return (this.get('rowIndex') % 2 == 0);
+    return (this.get('rowIndex') % 2 === 0);
   })
 });
