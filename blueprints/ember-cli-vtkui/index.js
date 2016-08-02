c88c41285438b30ab378736391cbce4fdeb48ed3
Ryan Connolly <rynam0@gmail.com>
2016-08-02 15:19:24 -0400

    update blueprint dependencies

--- blueprints/ember-cli-vtkui/index.js
+++ blueprints/ember-cli-vtkui/index.js
@@ -7,8 +7,8 @@ module.exports = {
         { name: 'ember-cli-selectize', target: '0.5.2' },
         { name: 'ember-cp-validations', target: '2.5.0' },
         { name: 'ember-tooltips', target: '0.5.8' },
-        { name: 'liquid-fire', target: '0.23.0' },
-        { name: 'ember-modal-dialog', target: '0.8.3' },
+        { name: 'liquid-fire', target: '0.24.0' },
+        { name: 'ember-modal-dialog', target: '0.8.8' },
         { name: 'ember-cli-sass', target: '5.3.1' },
         { name: 'ember-sortable', target: '1.8.1' },
         { name: 'ember-concurrency', target: '0.6.1'}
