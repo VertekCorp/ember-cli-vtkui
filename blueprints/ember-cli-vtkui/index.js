19d3f1d6447c2444cfa1963819aeb0f4c3ad794d
Lev Levit <llevit@vertekvt.net>
2020-01-24 11:27:03 -0500

    fixed space and tab error

--- blueprints/ember-cli-vtkui/index.js
+++ blueprints/ember-cli-vtkui/index.js
@@ -12,9 +12,9 @@ module.exports = {
         { name: 'ember-sortable', target: '2.1.1' },
         { name: 'ember-concurrency', target: '1.1.3'},
         { name: 'ember-pikaday', target: '2.4.1'},
-		    { name: "ember-paper", target: "^1.0.0-beta.25"},
-		    { name: "ember-light-table", target: "git+https://github.com/offirgolan/ember-light-table.git#2-x"},
-		    { name: "ember-font-awesome", target: "^4.0.0-rc.4"}
+        { name: "ember-paper", target: "^1.0.0-beta.25"},
+        { name: "ember-light-table", target: "git+https://github.com/offirgolan/ember-light-table.git#2-x"},
+        { name: "ember-font-awesome", target: "^4.0.0-rc.4"}
       ]
     });
   }
