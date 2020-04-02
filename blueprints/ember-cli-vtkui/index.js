9fb8ac452215e54637665bbc94ecc490677cbc98
Lev Levit <llevit@vertekvt.net>
2020-04-02 10:01:02 -0400

    exact versions of ember paper & ember-light-table w/o the '^'

--- blueprints/ember-cli-vtkui/index.js
+++ blueprints/ember-cli-vtkui/index.js
@@ -12,8 +12,8 @@ module.exports = {
         { name: 'ember-sortable', target: '2.1.1' },
         { name: 'ember-concurrency', target: '1.1.3'},
         { name: 'ember-pikaday', target: '2.4.1'},
-        { name: 'ember-paper', target: '^1.0.0-beta.25'},
-        { name: 'ember-light-table', target: '^2.0.0-beta.4'},
+        { name: 'ember-paper', target: '1.0.0-beta.25'},
+        { name: 'ember-light-table', target: '2.0.0-beta.4'},
         { name: 'ember-font-awesome', target: '^4.0.0-rc.4'},
         { name: 'ember-engines', target: '^0.8.5'},
         { name: 'ember-moment', target: '^8.0.0'}
